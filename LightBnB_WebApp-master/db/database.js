57160ea2f33c476040e33ec895e79729f1a719f5
Xyrelle Ebol <xyrelle.ebol@gmail.com>
2023-04-06 18:38:53 -0600

    Refactor more functions to include psql queries

--- LightBnB_WebApp-master/db/database.js
+++ LightBnB_WebApp-master/db/database.js
@@ -24,14 +24,19 @@ pool.query(
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithEmail = function(email) {
-  let resolvedUser = null;
-  for (const userId in users) {
-    const user = users[userId];
-    if (user?.email.toLowerCase() === email?.toLowerCase()) {
-      resolvedUser = user;
-    }
-  }
-  return Promise.resolve(resolvedUser);
+  return pool
+    .query(`SELECT * FROM users WHERE email = $1`, [email])
+    .then((result) => {
+      console.log(result.rows);
+      if (result && result.rows.length) {
+        return result.rows[0];
+      } else {
+        return null;
+      }
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 
 /**
@@ -40,7 +45,19 @@ const getUserWithEmail = function(email) {
  * @return {Promise<{}>} A promise to the user.
  */
 const getUserWithId = function(id) {
-  return Promise.resolve(users[id]);
+  return pool
+    .query(
+      `SELECT * FROM users WHERE id = $1`, [id])
+    .then((result) => {
+      if (result && result.rows.length) {
+        return result.rows[0];
+      } else {
+        return null;
+      }
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 
 /**
@@ -49,10 +66,21 @@ const getUserWithId = function(id) {
  * @return {Promise<{}>} A promise to the user.
  */
 const addUser = function(user) {
-  const userId = Object.keys(users).length + 1;
-  user.id = userId;
-  users[userId] = user;
-  return Promise.resolve(user);
+  // const userId = Object.keys(users).length + 1;
+  // user.id = userId;
+  // users[userId] = user;
+  // return Promise.resolve(user);
+  return pool
+    .query(`INSERT INTO users (name, email, password)
+    VALUES ($1, $2, $3) 
+    RETURNING *;`, [user.name, user.email, user.password])
+    .then((result) => {
+      console.log('result', result.rows);
+      return result.rows[0]
+    })
+    .catch((err) => {
+      console.log(err.message);
+    });
 };
 
 /// Reservations
